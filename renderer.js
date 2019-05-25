789fff74b9f6e83147057b492dea78f9a5ba8aa3
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-05-25 06:56:35 +0200

    Embed Leaflet.Draw

--- renderer.js
+++ renderer.js
@@ -8,3 +8,18 @@ var map = L.map('map').setView([51.505, -0.09], 13)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map)
+
+var drawnItems = new L.FeatureGroup()
+map.addLayer(drawnItems)
+var drawControl = new L.Control.Draw({
+  edit: {
+    featureGroup: drawnItems
+  }
+})
+map.addControl(drawControl)
+
+map.on(L.Draw.Event.CREATED, function (event) {
+  var layer = event.layer
+
+  drawnItems.addLayer(layer)
+})
